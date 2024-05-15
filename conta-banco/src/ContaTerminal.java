c1927e58f0730dadf11e3fd42647bc1493b1acbc
Glauco Silva <glaucocs@gmail.com>
2024-05-15 00:36:46 -0300

    Commit inicial. Recebe os dados do banco e cliente e imprime seu saldo.

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+import java.util.Locale;
+
 public class ContaTerminal {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o número da Agência: ");
+        String agencia = scanner.next();
+
+        System.out.println("Por favor, digite o número da sua conta corrente: ");
+        int numero = scanner.nextInt();
+
+        System.out.println("Por favor, digite o nome do cliente: ");
+        String nomeCliente = scanner.next();
+
+        double saldo = 12000.00;
+        
+        System.out.println("Olá " + nomeCliente + 
+        ", obrigado por criar uma conta em nosso banco, sua agência é " +
+        agencia + " conta " + numero + "e seu saldo " + String.format("%.2f", saldo) + 
+        " já está disponível para saque.");
+
+        scanner.close();
     }
 }
